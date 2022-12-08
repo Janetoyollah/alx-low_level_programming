6b21528258dcadfca2a9213872a42efccb957f54
Janetoyollah <119446988+Janetoyollah@users.noreply.github.com>
2022-12-09 00:37:21 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 /**
  * main -Printing a string using puts function
- *Description: This programme prints a string and a new line
- * Return: Always 0 
+ * Description: This programme prints a string and a new line
+ * Return: 0
  */
 int main(void)
 {
-	Char *s="\"Programming is like building a multilingual puzzle";
+	Char *s = "\"Programming is like building a multilingual puzzle";
 Puts (s);
 
-return(0);
+return (0);
 
 }	
