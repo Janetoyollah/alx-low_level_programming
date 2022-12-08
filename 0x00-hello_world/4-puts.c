e0930ed3f103f78860b9a864e582814f43b5a70b
Janetoyollah <119446988+Janetoyollah@users.noreply.github.com>
2022-12-08 23:41:40 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
+ * main -Printing a string using puts function
+ *Description: This programme prints a string and a new line
+ * Return: Always 0 
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	Char *s="\"Programming is like building a multilingual puzzle";
+Puts (s);
+
+return(0);
 
 }	
