f78f48d00e8d54e00237ca140b204752bf7b11a7
Janetoyollah <119446988+Janetoyollah@users.noreply.github.com>
2022-12-10 14:51:38 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *My hello, prinf task
+ *main - Entry point
+ *Description: my hello, prinf task
  *Return: return 1 after program execution
  */
 int main(void)
