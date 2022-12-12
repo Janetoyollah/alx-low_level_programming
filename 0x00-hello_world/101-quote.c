01e85a3523cdea41a9e22ff6b1088aa56dab7b47
Janetoyollah <adhiamb>
2022-12-12 08:53:23 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/**
+ * main -  prints the standard error sandwitched around the standard outpu/t
+ *
+ * Return: returns 1
+ */
+int main(void)
+{
+write(2, änd that piece of art is useful\"- Dora korpar, 2015-10-19\n",59);
+return (1);
+}
