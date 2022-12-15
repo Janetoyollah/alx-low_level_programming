fc1bf5d3438da74ccadb68ae44a43a2a90ed47bd
Janetoyollah <adhiamb>
2022-12-15 07:32:45 -0800

    task 0,task 1 and task2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * ,ain - prints putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	-putchar('_');
+	-putchar('p');
+	-putchar('_');
+	-putchar('_');
+	-putchar('_');
+	-putchar('_');
+	-putchar('_');
+	-putchar('_');
+	-putchar('_');
+	return (0);
+}	
